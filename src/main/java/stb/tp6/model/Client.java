4e806af94d2a9eb4bb8a38a802c0ce566adf5492
Quentin Brodier <quentin.brodier@gmail.com>
2016-04-25 12:12:13 +0200

    Modif client

--- src/main/java/stb/tp6/model/Client.java
+++ src/main/java/stb/tp6/model/Client.java
@@ -7,7 +7,7 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlElement;
 
-@XmlRootElement (name = "stb")
+@XmlRootElement (name = "client")
 @XmlAccessorType(XmlAccessType.NONE)
 public class Client {
 	
